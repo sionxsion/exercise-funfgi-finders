24fe54d5764a22510052eafaae525618875fa17f
Simon <simonudemy09@gmail.com>
2026-01-29 16:44:10 +0100

    cleaning log

--- main.js
+++ main.js
@@ -5,5 +5,4 @@ const primaryNav = document.querySelector("#primary-nav");
 navToggle.addEventListener("click", () => {
   const isNavOpen = navToggle.getAttribute("aria-expanded") === "true";
   navToggle.setAttribute("aria-expanded", !isNavOpen);
-  console.log(isNavOpen);
 });
